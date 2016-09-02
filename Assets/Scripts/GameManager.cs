c8579c68788cb65b2e3fec65bebb64e2f637f77c
pnisarg <pnisarg26@yahoo.com>
2016-09-02 01:49:29 -0700

    game completed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using System;
 
 public class GameManager : MonoBehaviour {
+	public Text continueText;
+	private float blinkTime = 0f;
+	private bool blink;
+	public Text ScoreText;
+	private float timeElapsed = 0f;
+	private float bestTime = 0f;
 
 	public GameObject playerPrefab;
 	private bool gameStarted;
@@ -9,6 +17,7 @@ public class GameManager : MonoBehaviour {
 	private GameObject player;
 	private GameObject floor;
 	private Spawner spawner;
+	private bool beatBestTime;
 
 	void Awake(){
 		floor = GameObject.Find ("Foreground");
@@ -26,6 +35,8 @@ public class GameManager : MonoBehaviour {
 
 		spawner.active = false;
 		Time.timeScale = 0;
+		continueText.text = "PRESS ANY BUTTON TO START";
+		bestTime = PlayerPrefs.GetFloat ("BestTime");
 	}
 	
 	// Update is called once per frame
@@ -36,6 +47,19 @@ public class GameManager : MonoBehaviour {
 				ResetGame ();
 			}
 		}
+		if (!gameStarted) {
+			blinkTime++;
+			if (blinkTime % 40 == 0) {
+				blink = !blink;
+			}
+			continueText.canvasRenderer.SetAlpha (blink ? 0 : 1);
+			var testColor = beatBestTime ? "#FF0" : "#FFF";
+
+			ScoreText.text = "TIME: " + FormatTime (timeElapsed) + "\n<color="+testColor+">BEST: " + FormatTime (bestTime) + "</color>";
+		} else {
+			timeElapsed += Time.deltaTime;
+			ScoreText.text = "TIME: " + FormatTime (timeElapsed);
+		}
 	}
 	void OnPlayerKilled(){
 		spawner.active = false;
@@ -45,6 +69,12 @@ public class GameManager : MonoBehaviour {
 		player.GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
 		timeManager.ManipulateTime (0, 5.5f);
 		gameStarted = false;
+		continueText.text = "PRESS ANY BUTTON TO RESTART";
+		if (timeElapsed > bestTime) {
+			bestTime = timeElapsed;
+			PlayerPrefs.SetFloat ("BestTime", bestTime);
+			beatBestTime = true;
+		}
 	}
 
 	void ResetGame(){
@@ -54,5 +84,15 @@ public class GameManager : MonoBehaviour {
 		var playerDestroyScript = player.GetComponent<DestroyOffScreen> ();
 		playerDestroyScript.DestroyCallback += OnPlayerKilled;
 		gameStarted = true;
+		continueText.canvasRenderer.SetAlpha (0);
+
+		timeElapsed = 0;
+		beatBestTime = false;
+	}
+
+	string FormatTime(float value){
+		TimeSpan t = TimeSpan.FromSeconds (value);
+
+		return string.Format ("{0:D2}:{1:D2}", t.Minutes, t.Seconds);
 	}
 }
